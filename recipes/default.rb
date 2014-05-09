1d90edce8232e8619a5d1142c007ec8c1c89c36d
Christopher Chow <christopher.chow@kinesis.org>
2014-05-09 16:40:15 +1000

    Set source dest check to false.

--- recipes/default.rb
+++ recipes/default.rb
@@ -58,3 +58,17 @@ include_recipe "iptables"
 iptables_rule "openvpn" do
   source "iptables/openvpn.erb"
 end
+
+include_recipe "awscli"
+
+instance_region = `curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | grep -Po "(us|sa|eu|ap)-(north|south)?(east|west)?-[0-9]+"`.strip
+instance_id = `curl -s http://169.254.169.254/latest/meta-data/instance-id`
+
+execute "Disable Source/Dest. check" do
+  command <<-EOH
+    aws ec2 --region "#{instance_region}" \
+      modify-instance-attribute \
+      --instance-id "#{instance_id}" \
+      --source-dest-check false
+  EOH
+end
